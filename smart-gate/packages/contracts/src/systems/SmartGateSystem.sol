fcb14da221040a9e12c5b6382742af8032ca3edd
CCP Red Dragon <181853434+CCP-Red-Dragon@users.noreply.github.com>
2025-01-02 15:01:49 +0000

    Security Changes (#74)
    * Added only owner
    
    * Import changes

--- smart-gate/packages/contracts/src/systems/SmartGateSystem.sol
+++ smart-gate/packages/contracts/src/systems/SmartGateSystem.sol
@@ -11,10 +11,23 @@ import { RESOURCE_SYSTEM } from "@latticexyz/world/src/worldResourceTypes.sol";
 import { CharactersTable } from "@eveworld/world/src/codegen/tables/CharactersTable.sol";
 import { GateAccess } from "../codegen/tables/GateAccess.sol";
 
+import { IERC721 } from "@eveworld/world/src/modules/eve-erc721-puppet/IERC721.sol";
+
+import { DeployableTokenTable } from "@eveworld/world/src/codegen/tables/DeployableTokenTable.sol";
+
 /**
  * @dev This contract is an example for implementing logic to a smart gate
  */
-contract SmartGateSystem is System {
+contract SmartGateSystem is System {  
+  /**
+   * @dev Only owner modifer
+   */
+  modifier onlyOwner(uint256 smartObjectId) {
+    address ssuOwner = IERC721(DeployableTokenTable.getErc721Address()).ownerOf(smartObjectId);
+    require(_msgSender() == ssuOwner, "Only owner can call this function");
+    _;
+  }
+
   function canJump(uint256 characterId, uint256 sourceGateId, uint256 destinationGateId) public view returns (bool) {
     //Get the allowed corp
     uint256 allowedCorp = GateAccess.get(sourceGateId);
@@ -30,7 +43,7 @@ contract SmartGateSystem is System {
     }    
   }
 
-  function setAllowedCorp(uint256 sourceGateId, uint256 corpID) public {
+  function setAllowedCorp(uint256 sourceGateId, uint256 corpID) public onlyOwner(sourceGateId) {
     GateAccess.set(sourceGateId, corpID);
   }
 }
