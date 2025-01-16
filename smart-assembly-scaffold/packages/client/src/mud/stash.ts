9093f91909fb8c7f0b72c7e51f377228525dd6fc
ccp sondheim <32130807+rhubarbgarden@users.noreply.github.com>
2025-01-16 15:49:52 +0000

    don't import mudconfig from @eveworld/world

--- smart-assembly-scaffold/packages/client/src/mud/stash.ts
+++ smart-assembly-scaffold/packages/client/src/mud/stash.ts
@@ -1,6 +1,6 @@
 import { createStash } from "@latticexyz/stash/internal";
 import config from "contracts/mud.config";
-import worldConfig from "@eveworld/world/mud.config";
+import worldConfig from "contracts/eveworld/mud.config";
 
 export const stash = createStash(config);
 export const worldStash = createStash(worldConfig);
